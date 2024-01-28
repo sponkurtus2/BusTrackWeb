b4775a9e23fb74958be27e52fe623ded4d291310
sponkurtus2 <a1122150057@utch.edu.mx>
2024-01-27 20:04:31 -0600

    fix?

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          <h1>Holi</h1>
         </p>
       </div>
       <p className="read-the-docs">
